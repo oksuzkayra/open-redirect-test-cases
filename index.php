ea7a8fa6e7bee432e282c3e5e02635c979f62853
oksuzkayra <oksuzkayra16@gmail.com>
2023-04-01 04:17:13 +0300

    uploading index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+    <form action="login.php?redirecturl=http://www.google.com" method="post">
+        Username: <input type="text" name="username" method="post"><br>
+        Password: <input type="password" name="password" method="post">
+        <input type="submit" value="Login">
+    </form>
+
+</body>
+</html>
