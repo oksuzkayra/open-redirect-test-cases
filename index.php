fb478d5bc16c2477b10c152283beff9842671062
oksuzkayra <oksuzkayra16@gmail.com>
2023-04-02 03:12:02 +0300

    commit

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <html>
 <body>
 
-    <form action="login.php?redirecturl=http://www.google.com" method="post">
-        Username: <input type="text" name="username" method="post"><br>
-        Password: <input type="password" name="password" method="post">
-        <input type="submit" value="Login">
-    </form>
+    <?php
+            $url = "http://127.0.0.1:9090";
+            $trueurl = $url."?login=true";
+            $fullpathurl = $url.$_SERVER['REQUEST_URI'];
 
+    ?>
+    <img src="laptop.jpg" alt="laptop"><br>
+    <a href="login.php?returnURL=<?php echo $url;?>">
+        <button>Satın Al</button>
+    </a>
+
+    <?php
+        $login= $_GET['login'];
+
+        if ($login== "true") {
+            echo "<h1>Purchased!</h1>";
+        }
+    ?>
+</script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,27 +4,40 @@
 
 
         <?php
+
+            $trueurl = "?login=true";
+            $fullpathurl = $url.$_SERVER['REQUEST_URI'];
             $usernames= array('admin',"test");
             $passwords= array("pwd", "test");
 
 
-            if ($_POST['username'] == $usernames[0] && $_POST['password'] == $passwords[0]) {
+            
+            if ($_REQUEST['username'] == $usernames[0] && $_REQUEST['password'] == $passwords[0]) {
                 
                 echo "<h1>Logged in as admin user! Redirection...</h1>";
-                header("Location: ".$_GET['redirecturl']);
+                sleep(2);
+                header("Location: ".$_REQUEST['returnURL'].$trueurl);
                 
 
                 
             }
-            else if ($_POST['username'] == $usernames[1] && $_POST['password'] == $passwords[1]) {
+            else if ($_REQUEST['username'] == $usernames[1] && $_REQUEST['password'] == $passwords[1]) {
                 echo "<h1>Logged in as test user! Redirection...</h1>";
-                header("Location: ".$_GET['redirecturl']);
+                sleep(2);
+                header("Location: ".$_REQUEST['returnURL'].$trueurl);
             }
-            else {
-                echo "<h1>Invalid error!</h1>";
-            }
+
 
         ?>
+
+        <h1>You must be logged in to purchase.</h1>
+        <form action="login.php?returnURL=<?php echo $_GET['returnURL']?>" method="post">
+
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Sign in">
+
+        </form>
 
 
     </body>
